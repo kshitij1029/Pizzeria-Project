ec7cd1895b354f933280af54300e047dfdf30ec1
Kshitij Jha <connect.kshitijjha@gmail.com>
2026-02-09 00:18:10 +0530

    third changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import About from './Components/About'
 import Hero from './Components/Hero'
 import Navbar from './Components/Navbar'
+import Orders from './Components/Orders'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar/>
       <Hero/>
       <About/>
+      <Orders/>
     </>
   )
 }
