bbd79246f3e79ceb29c013cd337258d6df6a91ab
Kshitij Jha <connect.kshitijjha@gmail.com>
2026-02-09 23:14:29 +0530

    fourth changes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -5,7 +5,7 @@ import about_img from '../assets/about-img.png'
 const About = () => {
   return (
     <div className='flex justify-evenly py-10 px-15'>
-        <img className='h-[380px] min-w-[326px] ' src={about_img}/>
+        <img className='h-95 min-w-81.5 ' src={about_img}/>
         <div className='flex flex-col px-10 justify-evenly pb-10'>
             <h1 className='text-[50px] font-bold bg-clip-text text-transparent bg-linear-to-r from-[#900] via-[#f00] to-[#f00]'>Dine-in to the Finest</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor impedit vitae sunt earum sequi consequatur voluptate fugit fugiat quo animi perferendis reprehenderit, nemo accusamus mollitia blanditiis? Quaerat laborum odit ullam possimus sapiente et similique, delectus aspernatur quae libero consectetur fugit illo enim quidem natus suscipit perspiciatis iusto est reprehenderit qui?</p>
--- src/Components/ExtraInfoCard.jsx
+++ src/Components/ExtraInfoCard.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const ExtraInfo = (props) => {
     return (
         <div className='flex flex-col justify-center items-center'>
-            <img className='h-[100px] w=[100px] contrast-200 m-4' src={props.extraImg}/>
+            <img className='h-25 w=[100px] contrast-200 m-4' src={props.extraImg}/>
             <h1 className='text-[24px] font-bold text-[#900]'>{props.heading}</h1>
             <p className='text-center'>{props.text}</p>
         </div>
