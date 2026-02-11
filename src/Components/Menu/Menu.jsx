8518f66d92ef87aeaccf5a7d5726bbdd55287b79
Kshitij Jha <connect.kshitijjha@gmail.com>
2026-02-11 17:30:33 +0530

    sixth changes

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import MenuSection from './MenuSection'
+
 
 const Menu = () => {
   return (
-    <div>
-        
+    <div className='flex flex-col items-center p-8'>
+        <h1 className='text-[28px] font-bold bg-clip-text text-transparent bg-linear-to-r from-[#900]  to-[#f00]'>OUR EXCLUSIVE MENU</h1>
+        <p className='text-[20px]'>Enjoy our vide range of Pizza's and other Snacks!!</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad culpa hic vel exercitationem, magni deleniti</p>
+        <MenuSection/>
     </div>
   )
 }
--- src/Components/Menu/MenuSection.jsx
+++ src/Components/Menu/MenuSection.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 
 const MenuSection = () => {
+  const [isFill, setIsFill] = useState(false);
+  const fill = ()=>{
+    if(!isFill) setIsFill(true);
+  }
+  const Unfill = ()=>{
+    if(isFill) setIsFill(false);
+  }
   return (
-    <div>
-        
+    <div className='mt-5'>
+      <div className='flex justify-evenly items-center'>
+        <button className='btn mx-5 shadow-sm shadow-[#900] py-2 px-6 rounded-3xl relative overflow-hidden z-20 transition-[0.5s] bg-transparent'>Veg</button>
+        <button className='btn mx-5 shadow-sm shadow-[#900] border-[#ffcc00] py-2 px-6 rounded-3xl relative overflow-hidden z-20 transition-[0.5s] bg-transparent'>Non-Veg</button>
+        <button className='btn mx-5 shadow-sm shadow-[#900] border-[#ffcc00] py-2 px-6 rounded-3xl relative overflow-hidden z-20 transition-[0.5s] bg-transparent'>Beverages</button>
+        <button className='btn mx-5 shadow-sm shadow-[#900] border-[#ffcc00] py-2 px-6 rounded-3xl relative overflow-hidden z-20 transition-[0.5s] bg-transparent'>Breads and Dips</button>
+        <button className='btn mx-5 shadow-sm shadow-[#900] border-[#ffcc00] py-2 px-6 rounded-3xl relative overflow-hidden z-20 transition-[0.5s] bg-transparent'>Deserts</button>
+        <button className='btn mx-5 shadow-sm shadow-[#900] border-[#ffcc00] py-2 px-6 rounded-3xl relative overflow-hidden z-20 transition-[0.5s] bg-transparent'>Specialized</button>
+      </div>
     </div>
   )
 }
