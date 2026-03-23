a0aa7372b101c37c2e9dd2d7e02f8c74ed7c7ddb
Kshitij Jha <connect.kshitijjha@gmail.com>
2026-03-24 00:21:10 +0530

    more commit

--- src/Components/Review/Dots2.jsx
+++ src/Components/Review/Dots2.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Dots2 = ({index}) => {
   return (
-    <div className='mt-3'>
+    <div className='mt-8'>
         <i className={`fa-solid fa-minus text-[20px] mx-0.5 ${index==0? "text-slate-600" : "text-slate-300"}`}></i>
         <i className={`fa-solid fa-minus text-[20px] mx-0.5 ${index==1? "text-slate-600" : "text-slate-300"}`}></i>
         <i className={`fa-solid fa-minus text-[20px] mx-0.5 ${index==2? "text-slate-600" : "text-slate-300"}`}></i>
